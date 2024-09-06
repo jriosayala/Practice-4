fe84700e792aeeebaa321433a02670219ff33921
Jose Rios <jriosayala@deloitte.com>
2024-09-06 13:13:44 -0600

    chore: create Models for each table

---
+++ Models/Purchase.cs
@@ -0,0 +1,10 @@
+namespace Practice_4.models;
+public class Purchase
+{
+    public int PurchaseID { get; set; }
+    public int ProductID { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public int QuantityPurchased { get; set; }
+    public decimal PurchasePrice { get; set; }
+    public Product Product { get; set; }
+}
---
+++ Models/Sale.cs
@@ -0,0 +1,10 @@
+namespace Practice_4.models;
+public class Sale
+{
+    public int SaleID { get; set; }
+    public int ProductID { get; set; }
+    public DateTime SaleDate { get; set; }
+    public int QuantitySold { get; set; }
+    public decimal SalePrice { get; set; }
+    public Product Product { get; set; }
+}
